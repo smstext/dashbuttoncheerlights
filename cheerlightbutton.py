180db901bdfbe4ac54cefbeac21e9159f12ad183
smstext <smstextaddict@gmail.com>
2018-07-28 21:04:47 +0100

    Add files via upload
    slight change to code, now retweets another random colour and keeps going till it crashes on selecting the same colour again. also get the following every so often:      
    arp_detailed = struct.unpack('2s2s1s1s2s6s4s6s4s', arp_header)
    struct.error: unpack requires a string argument of length 28

--- cheerlightbutton.py
+++ cheerlightbutton.py
@@ -3,42 +3,42 @@
 import socket
 import struct
 import binascii
+import random
 import datetime
 from time import sleep
-from random import choice
 import tweepy
 
-consumer_key        = 'enter here'
-consumer_secret     = 'enter here'
-access_token        = 'enter here'
-access_token_secret = 'enter here'
+consumer_key        = 'a'
+consumer_secret     = 'b'
+access_token        = 'c'
+access_token_secret = 'd'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-message = ['UB40 Red Red wine #cheerlights',
-           'Orange origina #cheerlights',
-	   'Lets follow the yellow brick road #cheerlights',
-           'Going rather green #cheerlights',
-           'Look at the blue sky #cheerlights',
-           'some wonderful violet flowers #cheerlights',
-           'white like a ghost #cheerlights',
-           'Cyan cat is coming #cheerlights',
-           'my face is going purple #cheerlights',
-           'magenta, what kinda colour is that #cheerlights',
-           'my nan had oldlace #cheerlights',
-           'tickle me pink #cheerlights',
-           'warmwhite is that such a colour? #cheerlights',]
-
-my_message = choice(message)
+messages = [
+    "UB40 Red Red wine #cheerlights",
+    "Orange origina #cheerlights",
+    "Lets follow the yellow brick road #cheerlights",
+    "Going rather green #cheerlights",
+    "Look at the blue sky #cheerlights",
+    "some wonderful violet flowers #cheerlights",
+    "white like a ghost #cheerlights",
+    "Cyan cat is coming #cheerlights",
+    "my face is going purple #cheerlights",
+    "magenta, what kinda colour is that #cheerlights",
+    "my nan had oldlace #cheerlights",
+    "tickle me pink #cheerlights",
+    "warmwhite is that such a colour? #cheerlights",
+]
 
 rawSocket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW,
                           socket.htons(0x0003))
-MAC = 'dashmac here'
+
+MAC = 'fc--------'
 
 while True:
     packet = rawSocket.recvfrom(2048)
-
     ethernet_header = packet[0][0:14]
     ethernet_detailed = struct.unpack('!6s6s2s', ethernet_header)
 
@@ -55,8 +55,8 @@ while True:
 
     if source_mac == MAC:
         print "Dash button pressed!, IP = " + dest_ip
+        my_message = random.choice(messages)
         api.update_status(my_message)
+
         print (my_message)
-
-
 
